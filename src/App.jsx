7e85f94a3bec129f11143ced593f0a64d012158d
kalyan <k.softech.ui@gmail.com>
2021-07-16 12:12:13 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import New from './new'
+import Sample from './sample';
 
 const App = () =>{
     
@@ -7,6 +8,7 @@ const App = () =>{
         <>
         <div className="App">
             <New />
+            <Sample />
         </div>
         
         </>
